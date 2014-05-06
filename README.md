b1a7650ac1589f5d7d78d46d9edf1956ed9f62ef
Paul Demers <paul@demersdesigns.com>
2014-05-06 15:09:26 -0400

    2.0 release

--- README.md
+++ README.md
@@ -3,25 +3,28 @@
 A sensible baseline for simple web projects using [HTML5 Boilerplate](https://github.com/h5bp/html5-boilerplate), [SASS](http://sass-lang.com/), [Bourbon](http://bourbon.io/), [Neat](http://neat.bourbon.io/), and [Gulp](http://gulpjs.com/).
 ##Requirements:
 ---
-* [Node.js](http://nodejs.org/download)
-* npm: `$ npm install npm`
-* If you get an installation error, you may need to set the owership of the /usr/local folder to the current user. If so: run `sudo chown -R $USER /usr/local`
+* [Homebrew](http://brew.sh/)
+* [Node.js](http://nodejs.org) & [NPM](https://www.npmjs.org/)
 * [Livereload Chrome Extension](http://goo.gl/bkMepd)
 
 ##Project Capabilities:
 ---
-* JS - Javascript will be run through JS Hint and return any errors to the console. If there are no errors, the Javascript files will be concatenated and saved to the JS folder in the top-level directory.
-* CSS - SCSS files will be concatenated together and saved in the CSS folder in the top-level directory.
-* INC and HTML - HTML files and any includes that are present will be compiled and saved to the HTML folder in the top-level directory.
+* JS - Javascript will be run through JS Hint and return any errors to the console. If there are no errors, the Javascript files will be concatenated and saved to the JS folder in the dist directory.
+* CSS - SCSS files will be concatenated together and saved in the CSS folder in the dist directory.
+* INC and HTML - HTML files and any includes that are present will be compiled and saved to the HTML folder in the dist directory.
 * Any HTML, INC, SCSS, or JS file updates will trigger a reload of the browser.
 * Errors in JS and CSS will output to the command line.
 
 ##Install & Usage:
 ---
-1. Make sure that Node.js, and NPM are installed. To verify this, you can type `which node` and `which npm` into the command line.
-2. Install the Livereload Chrome extension from the link above.
-3. From the command line, CD into the directory where you pulled this repo and run `$ npm install`, which will download all of the Gulp dependencies listed above into a node-modules folder. This may take a minute or two to complete.
-4. From the command line, type in `$ Gulp` to fire up the Gulp instance. If you get an error, you may need to install Gulp globally. To do this type `npm install gulp -g` into the command line. When this completes, try tying in `gulp` into the command line once again.
-5. In Chrome, open up the HTML file you want to work with. You don't need to be running MAMP unless there are PHP, database, or other back-end dependencies.
-5. In Chrome, click the Livereload extension button in the upper right hand side of the browser to activate it. The center of the icon should fill in with black.
-6. From here, you should be able to make changes to the HTML, SCSS, and JS source code in the assets folder, which will compile out production-ready files to the main site directory. You should then see the updates you made reflected in the browser without reloading.
+1. If you don't have Homebrew, Node, and NPM installed please complete the following:
+<br>
+a. Open up the terminal and type `ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"`
+b. Use homebrew to install Node, which includes NPM `brew install node`
+2. Make sure that Node.js, and NPM are installed. To verify this, you can type `which node` and `which npm` into the command line. If you get an installation error, you may need to set the owership of the /usr/local folder to the current user. If so: run `sudo chown -R $USER /usr/local`
+3. Install the Livereload Chrome extension from the link above.
+4. From the command line, CD into the directory where you pulled this repo and run `$ npm install`, which will download all of the Gulp dependencies listed above into a node-modules folder. This may take a minute or two to complete.
+5. From the command line, type in `gulp watch` to fire up the Gulp instance. If you get an error, you may need to install Gulp globally. To do this type `npm install gulp -g` into the command line. When this completes, try tying in `gulp watch` into the command line once again.
+6. In Chrome, open up the HTML file you want to work with. You don't need to be running MAMP unless there are PHP, database, or other back-end dependencies.
+7. In Chrome, click the Livereload extension button in the upper right hand side of the browser to activate it. The center of the icon should fill in with black.
+8. You should do all of your edits to the HTML, SCSS, JS, and HTML within the **assets** folder, which will compile out production-ready files to the **dist** directory. You should then see the updates you made reflected in the browser without reloading.
--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,185 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var stylish = require('jshint-stylish');
+// var gulp = require('gulp');/
+// var gutil = require('gulp-util');/
+// var stylish = require('jshint-stylish');/
 
 //plugins
-var concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
-    sass = require('gulp-sass'),
-    prefix = require('gulp-autoprefixer'),
-    minifyHtml = require('gulp-minify-html'),
-    minifyCSS = require('gulp-minify-css'),
-    imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache'),
-    map = require('map-stream'),
-    livereload = require('gulp-livereload'),
-    include = require('gulp-include'),
-    wait = require('gulp-wait'),
-    lr = require('tiny-lr'),
-    server = lr();
+// var concat = require('gulp-concat'),/
+//     uglify = require('gulp-uglify'),/
+//     jshint = require('gulp-jshint'),/
+//     sass = require('gulp-sass'),/
+//     prefix = require('gulp-autoprefixer'),/
+//     minifyHtml = require('gulp-minify-html'),
+//     minifyCSS = require('gulp-minify-css'),/
+//     imagemin = require('gulp-imagemin'),/
+//     cache = require('gulp-cache'),/
+//     map = require('map-stream'),
+//     livereload = require('gulp-livereload'),/
+//     include = require('gulp-include'),/
+//     wait = require('gulp-wait'),/
+//     lr = require('tiny-lr'),
+//     server = lr();
+
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		autoprefix = require('gulp-autoprefixer'),
+		minifyCSS = require('gulp-minify-css'),
+		stylish = require('jshint-stylish'),
+		jshint = require('gulp-jshint'),
+		uglify = require('gulp-uglify'),
+		concat = require('gulp-concat'),
+		imagemin = require('gulp-imagemin'),
+		include = require('gulp-include'),
+		rename = require('gulp-rename'),
+		cache = require('gulp-cache'),
+		livereload = require('gulp-livereload'),
+		wait = require('gulp-wait'),
+		notify = require('gulp-notify');
+
+//Vars for file locations and output destinations
+var cssSrc = 'assets/sass/style.scss',
+		cssDist = 'dist/css',
+		htmlSrc = 'assets/html/**/*.html',
+		htmlDist = 'dist',
+		imageSrc = 'assets/img/**/*',
+		imageDist = 'dist/img',
+		jsSrc = 'assets/js/**/*',
+		jsDist = 'dist/js';
+
+//Compile sass, autoprefix, output non-minified version, output minified version,
+//notify the OS
+gulp.task('styles', function(){
+	return gulp.src(cssSrc)
+		.pipe(sass({outputStyle: 'expanded', errLogToConsole: true}))
+		.pipe(autoprefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest(cssDist))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest(cssDist))
+		.pipe(notify({onLast: true, message: 'CSS compiled and minified!'}))
+});
+
+//Compile HTML Includes
+gulp.task('html', function(){
+	return gulp.src(htmlSrc)
+		.pipe(include())
+    .pipe(gulp.dest(htmlDist))
+    .pipe(notify({onLast: true, message: "HTML includes compiled!"}))
+});
+
+//Optimize images
+gulp.task('images', function(){
+	return gulp.src(imageSrc)
+		.pipe(cache(imagemin()))
+    .pipe(gulp.dest(imageDist))
+    .pipe(notify({onLast: true, message: "Images crunched!"}))
+});
+
+gulp.task('scripts', function(){
+	return gulp.src(jsSrc)
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish))
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest(jsDist))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest(jsDist))
+		.pipe(notify({onLast: true, message: "JS linted, concatenated, and minfied!"}))
+});
+
+//Run the tasks listed above
+gulp.task('default', function(){
+	gulp.start('styles', 'html', 'images', 'scripts');
+});
+
+//Watch for changes and reload the page
+gulp.task('watch', function(){
+	gulp.watch(cssSrc, ['styles']);
+	gulp.watch(htmlSrc, ['html']);
+	gulp.watch(imageSrc, ['images']);
+	gulp.watch(jsSrc, ['scripts']);
+
+	var server = livereload();
+
+	gulp.watch(['dist/**']).on('change', function(file){
+		server.changed(file.path);
+	});
+});
 
 // tasks
-gulp.task('build', function(){
+// gulp.task('build', function(){
  
-  // { concat, minify & jshint }
-  var scriptFiles = './assets/js/**/*a.js';
-  var scriptDist = './js';
+//   // { concat, minify & jshint }
+//   var scriptFiles = './assets/js/**/*a.js';
+//   var scriptDist = './js';
   
-  gulp.src(scriptFiles)
-      .pipe(jshint())
-      .pipe(jshint.reporter(stylish))
-      .pipe(concat('all.min.js'))
-      .pipe(uglify({outSourceMap: true}))
-      .pipe(gulp.dest(scriptDist))
-      .pipe(notify({onLast: true, message: "JS compiled!"}))
-      .pipe(wait(100))
-      .pipe(livereload(server));
+//   gulp.src(scriptFiles)
+//       .pipe(jshint())
+//       .pipe(jshint.reporter(stylish))
+//       .pipe(concat('all.min.js'))
+//       .pipe(uglify({outSourceMap: true}))
+//       .pipe(gulp.dest(scriptDist))
+//       .pipe(notify({onLast: true, message: "JS compiled!"}))
+//       .pipe(wait(100))
+//       .pipe(livereload(server));
 
-  // { image optimizer }
-  var imageFiles = './assets/img/**/*';
-  var imageDist = './img';
-  gulp.src(imageFiles)
-        .pipe(cache(imagemin()))
-        .pipe(gulp.dest(imageDist))
-        .pipe(notify({onLast: true, message: "Images crunched!"}))
-        .pipe(wait(100))
-        .pipe(livereload(server));
+//   // { image optimizer }
+//   var imageFiles = './assets/img/**/*';
+//   var imageDist = './img';
+//   gulp.src(imageFiles)
+//         .pipe(cache(imagemin()))
+//         .pipe(gulp.dest(imageDist))
+//         .pipe(notify({onLast: true, message: "Images crunched!"}))
+//         .pipe(wait(100))
+//         .pipe(livereload(server));
 
-  // { html }
-  var includes = './assets/html/**/*.html';
-  var includesDist = './';
+//   // { html }
+//   var includes = './assets/html/**/*.html';
+//   var includesDist = './';
 
-  gulp.src(includes)
-    .pipe(include())
-    .pipe(gulp.dest(includesDist))
-    .pipe(notify({onLast: true, message: "HTML includes compiled!"}))
-    .pipe(wait(100))
-    .pipe(livereload(server));
+//   gulp.src(includes)
+//     .pipe(include())
+//     .pipe(gulp.dest(includesDist))
+//     .pipe(notify({onLast: true, message: "HTML includes compiled!"}))
+//     .pipe(wait(100))
+//     .pipe(livereload(server));
 
-  // { sass }
-  var sassFiles = './assets/sass/style.scss';
-  var sassDist = './css';
+//   // { sass }
+//   var sassFiles = './assets/sass/style.scss';
+//   var sassDist = './css';
 
-  gulp.src(sassFiles)
-      .pipe(concat('style.scss'))
-      .pipe(sass({outputStyle: 'nested', errLogToConsole: true}))
-      .pipe(prefix("last 2 versions", "ie 8"))
-      //.pipe(minifyCSS())
-      .pipe(gulp.dest('./css'))
-      .pipe(concat('style.min.css'))
-      .pipe(minifyCSS())
-      .pipe(gulp.dest('./css'))
-      .pipe(notify({onLast: true, message: "Sass compiled!"}))
-      .pipe(wait(100))
-      .pipe(livereload(server));
-});
+//   gulp.src(sassFiles)
+//       .pipe(concat('style.scss'))
+//       .pipe(sass({outputStyle: 'nested', errLogToConsole: true}))
+//       .pipe(prefix("last 2 versions", "ie 8"))
+//       //.pipe(minifyCSS())
+//       .pipe(gulp.dest('./css'))
+//       .pipe(concat('style.min.css'))
+//       .pipe(minifyCSS())
+//       .pipe(gulp.dest('./css'))
+//       .pipe(notify({onLast: true, message: "Sass compiled!"}))
+//       .pipe(wait(100))
+//       .pipe(livereload(server));
+// });
 
-gulp.task('lr-server', function() {  
-  server.listen(35729, function(err) {
-    if(err) return console.log(err);
-  })
-});
+// gulp.task('lr-server', function() {  
+//   server.listen(35729, function(err) {
+//     if(err) return console.log(err);
+//   })
+// });
 
 
-// The default task (called when you run `gulp`)
-gulp.task('default', function() {
-  gulp.start('build', 'lr-server');
+// // The default task (called when you run `gulp`)
+// gulp.task('default', function() {
+//   gulp.start('build', 'lr-server');
 
-  // Watch files and run tasks if they change
-  gulp.watch('./assets/**/*', function() {
-    var date = new Date(), hour = date.getHours(), minutes = date.getMinutes(), seconds = date.getSeconds(),
-        buildTime = hour + ':' + minutes + ':' + seconds;
+//   // Watch files and run tasks if they change
+//   gulp.watch('./assets/**/*', function() {
+//     var date = new Date(), hour = date.getHours(), minutes = date.getMinutes(), seconds = date.getSeconds(),
+//         buildTime = hour + ':' + minutes + ':' + seconds;
 
-    gulp.start('build', function() {
-      gutil.log(gutil.colors.blue('------------- Built! -------------'), gutil.colors.green('( Last time -', buildTime, ')'));
-    });
+//     gulp.start('build', function() {
+//       gutil.log(gutil.colors.blue('------------- Built! -------------'), gutil.colors.green('( Last time -', buildTime, ')'));
+//     });
 
-  });
-});
+//   });
+// });
